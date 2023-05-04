604bf128f7e49216a84e24b57a3b2f58926923e6
Eddy A. Renteria <90665395+co1424@users.noreply.github.com>
2023-05-04 13:25:40 -0600

    Main Update

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import arcade
+import arcade.gui
+from menu import MainView
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Starting Template"
 
 
-class MyGame(arcade.Window):
+class MyGame(arcade.View):
     """
     Main application class.
 
@@ -17,7 +19,8 @@ class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
 
-        arcade.set_background_color(arcade.color.AMAZON)
+        #Menus.start_menu(self)
+        MainView(arcade.View)
 
         # If you have sprite lists, you should create them here,
         # and set them to None
@@ -25,7 +28,7 @@ class MyGame(arcade.Window):
     def setup(self):
         """ Set up the game variables. Call to re-start the game. """
         # Create your sprites and sprite lists here
-        pass
+        
 
     def on_draw(self):
         """
@@ -35,6 +38,7 @@ class MyGame(arcade.Window):
         # This command should happen before we start drawing. It will clear
         # the screen to the background color, and erase what we drew last frame.
         self.clear()
+
 
         # Call draw() on all your sprite lists below
 
@@ -82,10 +86,13 @@ class MyGame(arcade.Window):
 
 def main():
     """ Main function """
-    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    game.setup()
+    #game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    #game.setup()
+    #arcade.run()
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, resizable=True)
+    main_view = MainView()
+    window.show_view(main_view)
     arcade.run()
-
 
 if __name__ == "__main__":
     main()
