5dc6e6989487029f059d4475a5abd4341aae374e
lula314 <104175188+lula314@users.noreply.github.com>
2023-05-09 00:23:06 -0600

    update

--- Scene.py
+++
@@ -1,9 +0,0 @@
-import arcade
-
-class Scene:
-    def __init__(self, output_service):
-        self.__output_service = output_service
-        self.entity_list = []
-
-    def draw_scene(self):
-        
