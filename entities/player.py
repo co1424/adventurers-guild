b244892fe9410c1c01e5fd01648c6e43861be951
lula314 <104175188+lula314@users.noreply.github.com>
2023-06-01 14:55:25 -0600

    player and basic enemy textures updated

--- entities/player.py
+++ entities/player.py
@@ -7,11 +7,11 @@ class Player(Entity):
 
     def __init__(self):
 
-        folder = "Sprite/Player"
-        file_prefix = "character"
+        folder = "assets/Sprite/Player"
+        file_prefix = "player"
 
         # Set up parent class
-        scale = .1
+        scale = 1
 
         super().__init__(folder, file_prefix, scale)
 
