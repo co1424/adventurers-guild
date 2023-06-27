1c81956c32e1836243b84c07421c0e69d10c71c7
lula314 <104175188+lula314@users.noreply.github.com>
2023-06-27 14:16:31 -0600

    door fix and sprite updates

--- entities/health_boost.py
+++ entities/health_boost.py
@@ -3,6 +3,6 @@ from entities.entity import Entity
 
 class Health_Boost(Entity):
     def __init__(self):
-        scale = 1
+        scale = 1.5
         # Set up parent class
         super().__init__("assets/Sprite", "health_boost", scale)
--- entities/key.py
+++ entities/key.py
@@ -3,6 +3,6 @@ from entities.entity import Entity
 
 class Key(Entity):
     def __init__(self):
-        scale = 1
+        scale = 1.5
         # Set up parent class
-        super().__init__("assets/Sprite", "key", scale)
+        super().__init__("assets/Sprite", "enter_key", scale)
