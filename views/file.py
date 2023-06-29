578c1e4f095ce6cffcd5fb4437c3e4bf8a5c106a
Auzfest <114009523+Auzfest@users.noreply.github.com>
2023-06-29 13:01:34 -0600

    creates a new save.json if none exists

--- views/file.py
+++ views/file.py
@@ -1,10 +1,19 @@
 import json
+import os
 
 class file:
     def read_from_file(filename):
         filer_name = "save.json"
-        with open(filer_name, "r") as file:
-            data = json.load(file)
+        isFile = os.path.isfile(filename)
+        if (not isFile):
+            new_num = 0
+            with open('save.json', 'w') as f:
+                json.dump(new_num, f)
+                print("new save.json created")
+                return new_num
+        else:
+            with open(filer_name, "r") as file:
+                data = json.load(file)
         return data
 
     def save_to_file(save):
